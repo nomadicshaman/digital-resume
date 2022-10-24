df3c578a55e81293f51187c1732aa7d077b76131
BurakBozan <burakbozantr@gmail.com>
2022-10-24 16:19:15 +0900

    add theme buttons

---
+++ mainscript.js
@@ -0,0 +1,16 @@
+let root = document.documentElement;
+function lightTheme () {
+    root.style.setProperty('--mainTextColor', 'var(--mainTextColor-light)');
+    root.style.setProperty('--secondaryTextColor', 'var(--secondaryTextColor-light)');
+    root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-light)');
+    root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-light)');
+    root.style.setProperty('--mainBgColor', 'var(--mainBgColor-light)');
+}
+
+function darkTheme () {
+    root.style.setProperty('--mainTextColor', 'var(--mainTextColor-dark)');
+    root.style.setProperty('--secondaryTextColor', 'var(--secondaryTextColor-dark)');
+    root.style.setProperty('--mainLinkColor', 'var(--mainLinkColor-dark)');
+    root.style.setProperty('--mainBorderColor', 'var(--mainBorderColor-dark)');
+    root.style.setProperty('--mainBgColor', 'var(--mainBgColor-dark)');
+}
